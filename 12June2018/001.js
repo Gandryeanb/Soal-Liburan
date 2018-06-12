6921ed02f365d90322ed4df38a798a5a782357b8
gandryeanb <gstandrianb@gmail.com>
2018-06-13 03:26:32 +0700

    adding task 001

---
+++ 12June2018/001.js
@@ -0,0 +1,34 @@
+function soal1(param)
+{
+    var result = [] 
+
+    for (var i = param.length-1 ; i >= 0; i--) {
+      if (param[i] !== " ") {
+        console.log(param[i])
+      }
+    }
+}
+
+console.log(soal1("Aries Dimas Yudhistira"));
+/*
+    a
+    r
+    i
+    t
+    s
+    i
+    h
+    d
+    u
+    Y
+    s
+    a
+    m
+    i
+    D
+    s
+    e
+    i
+    r
+    A
+*/
